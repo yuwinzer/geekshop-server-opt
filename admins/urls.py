b70c22cb7eb0363e2c59d43bb0cd98a78a386eeb
yuwinzer <yu.moskalev@gmail.com>
2021-11-09 16:02:03 +0200

    added categories and products pages in admins app

--- admins/urls.py
+++ admins/urls.py
@@ -1,12 +1,23 @@
 from django.urls import path
-from admins.views import index, UserListView, UserCreateView, UserUpdateView, UserDeleteView
+import admins.views as admins
 
 app_name = 'admins'
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('users/', UserListView.as_view(), name='admin_users'),
-    path('users-create/', UserCreateView.as_view(), name='admin_users_create'),
-    path('users-update/<int:pk>/', UserUpdateView.as_view(), name='admin_users_update'),
-    path('users-delete/<int:pk>/', UserDeleteView.as_view(), name='admin_users_delete')
+    path('', admins.index, name='index'),
+    path('users/', admins.UserListView.as_view(), name='admin_users'),
+    path('users-create/', admins.UserCreateView.as_view(), name='admin_users_create'),
+    path('users-update/<int:pk>/', admins.UserUpdateView.as_view(), name='admin_users_update'),
+    path('users-delete/<int:pk>/', admins.UserDeleteView.as_view(), name='admin_users_delete'),
+
+    path('categories/create/', admins.category_create, name='category_create'),
+    path('categories/read/', admins.categories, name='categories'),
+    path('categories/update/<int:pk>/', admins.category_update, name='category_update'),
+    path('categories/delete/<int:pk>/', admins.category_delete, name='category_delete'),
+
+    path('products/create/category/<int:pk>/', admins.product_create, name='product_create'),
+    path('products/read/category/<int:pk>/', admins.CategoryProductsReadView.as_view(), name='products'),
+    path('products/read/<int:pk>/', admins.CategoryProductsReadView.as_view(), name='product_read'),
+    path('products/update/<int:pk>/', admins.product_update, name='product_update'),
+    path('products/delete/<int:pk>/', admins.product_delete, name='product_delete'),
 ]
